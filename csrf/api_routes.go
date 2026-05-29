e7cc40ada1105385fcad307965b6828a88dd70c5
Vinh Vo <vohuynhvinh@gmail.com>
2026-05-29 22:35:38 +0700

    Fix bug SCRF routing path

--- csrf/api_routes.go
+++ csrf/api_routes.go
@@ -1,22 +1,12 @@
 package csrf
 
 import (
-	"fmt"
 	"github.com/gflydev/core"
-	"github.com/gflydev/core/utils"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 // RegisterApi func for describe a group of API routes.
 func RegisterApi(apiRouter *core.Group) {
-	prefixAPI := fmt.Sprintf(
-		"/%s/%s",
-		utils.Getenv("API_PREFIX", "api"),
-		utils.Getenv("API_VERSION", "v1"),
-	)
-
 	// API Routers
-	apiRouter.Group(prefixAPI, func(apiRouter *core.Group) {
-		// CSRF token endpoint
-		apiRouter.GET("/csrf", NewCsrfTokenApi())
-	})
+	apiRouter.GET("/csrf", NewCsrfTokenApi())
 }
